70c6c3db1ce82acf84ec1ebd29a89fdc450e25f3
ahlam <ahlamahmed21101998@gmail.com>
2023-08-18 14:32:23 +0200

    finish liveaboard

--- src/app/liveabroad/liveabroad.component.ts
+++ src/app/liveabroad/liveabroad.component.ts
@@ -7,10 +7,30 @@ import { Component } from '@angular/core';
 })
 export class LiveabroadComponent {
 
+  // cards = [
+  //   { image: '../../assets/1.jpg', title: 'Card 1', content: 'This is the content of Card 1.' },
+  //   { image: '../../assets/2.jpg', title: 'Card 2', content: 'This is the content of Card 2.' },
+  //   { image: '../../assets/3.jpg', title: 'Card 3', content: 'This is the content of Card 3.' }
+  // ];
+
+
+
   cards = [
-    { image: '../../assets/1.jpg', title: 'Card 1', content: 'This is the content of Card 1.' },
-    { image: '../../assets/2.jpg', title: 'Card 2', content: 'This is the content of Card 2.' },
-    { image: '../../assets/3.jpg', title: 'Card 3', content: 'This is the content of Card 3.' }
+    {
+      imageUrl: '../../assets/ship.jpg',
+      title: 'LIVEABOARD SATAYA DOLPHIN HOUSE',
+      description: 'Marsa ALAM'
+    },
+    {
+      imageUrl: '../../assets/ship.jpg',
+      title: 'LIVEABOARD SATAYA DOLPHIN HOUSE',
+      description: 'Marsa ALAM'
+    },
+    {
+      imageUrl: '../../assets/ship.jpg',
+      title: 'LIVEABOARD SATAYA DOLPHIN HOUSE',
+      description: 'Marsa ALAM'
+    }
   ];
 
 }
